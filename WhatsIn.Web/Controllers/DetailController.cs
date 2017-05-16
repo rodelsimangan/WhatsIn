ca97a5ee24a8d94f26ad81c17a3f67dbfa5254aa
rodsim <rodel.simangan@walkerscott.co>
2017-05-16 23:15:06 +1000

    Detail updates

--- WhatsIn.Web/Controllers/DetailController.cs
+++ WhatsIn.Web/Controllers/DetailController.cs
@@ -15,19 +15,87 @@ namespace WhatsIn.Web.Controllers
 {
     public class DetailController : WhatsInControllerBase
     {
-        private readonly IStoreAppService _appService;
+        private readonly IStoreAppService _storeAppService;
+        private readonly ILocationAppService _locationAppService;
+        private readonly IProvinceAppService _provinceAppService;
 
-        public DetailController(IStoreAppService appService)
+        public DetailController(IStoreAppService appService, ILocationAppService locationAppService, IProvinceAppService provinceAppService)
         {
-            _appService = appService;
+            _storeAppService = appService;
+            _locationAppService = locationAppService;
+            _provinceAppService = provinceAppService;
         }
 
         public async Task<ActionResult> Index()
         {
-            StoreViewModel model = new StoreViewModel(new StoreDto());
-            var output = await _appService.GetStoreByUser(AbpSession.UserId.Value);
-            model = new StoreViewModel(output);
-            return View(model);
-        }      
+            try
+            {
+                await GetProvinceDropdown();
+                ViewData["locations"] = new List<LocationDto>();
+                StoreViewModel model = new StoreViewModel(new StoreDto());
+                var output = await _storeAppService.GetStoreByUser(AbpSession.UserId.Value);
+                model = new StoreViewModel(output);
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task GetProvinceDropdown()
+        {
+            try
+            {
+                var output = await _provinceAppService.GetProvinces(null, false);
+                ViewData["provinces"] = output;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task GetLocationsDropdown(int? provinceId)
+        {
+            try
+            {
+                if (provinceId.HasValue)
+                {
+                    var output = await _locationAppService.GetLocations(provinceId, null, false);
+                    ViewData["locations"] = output;
+                }
+                else
+                {
+                    ViewData["locations"] = new List<LocationDto>();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<ActionResult> _LocationsDropdown(int? provinceId)
+        {
+            try
+            {
+                if (provinceId.HasValue)
+                {
+                    var output = await _locationAppService.GetLocations(provinceId, null, false);
+                    ViewData["locations"] = output;
+                }
+                else
+                {
+                    ViewData["locations"] = new List<LocationDto>();
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
--- WhatsIn.Web/Views/Detail/Index.js
+++ WhatsIn.Web/Views/Detail/Index.js
@@ -15,18 +15,19 @@
  //           alert("SaveDetails");
 
             var obj = _$form.serializeFormToObject();
-
+            var dropdown = $("#ddlLocations").data("ejDropDownList");
+            obj.locationId = dropdown.option("value")
+            console.log(JSON.stringify( obj));
             //_modalManager.setBusy(true);
-            _appService.upsertStore(obj).done(function () {
+             _appService.upsertStore(obj).done(function () {
                 //_modalManager.close();
                 location.reload();
             }).always(function () {
                 abp.ui.setBusy('#DetailForm');
-            });
+            }); 
         });
 
-
-
+      
       /*   var _upsertGalleryModal = new app.ModalManager({
             viewUrl: abp.appPath + 'Galleries/UpsertGalleryModal',
             scriptUrl: abp.appPath + 'Views/Galleries/_UpsertGalleryModal.js',
@@ -64,9 +65,26 @@
                 }
             );
         }); */
+       
 
       
     });
 
-
+  
+   
 })();
+
+function onProvinceChange(args) {
+    $.ajax({
+        type: "POST",
+        url: DetailsUrlSettings.LocationsUrl,
+        data: { provinceId: args.value },   // <== change is here
+        success: function (response) {
+            console.log(response);
+            console.log(DetailsUrlSettings.LocationsDataSource);
+            var dropdown = $("#ddlLocations").data("ejDropDownList");
+            dropdown.option("dataSource", DetailsUrlSettings.LocationsDataSource);
+            $('#DIV-LOCATIONS').html(response);
+        },
+    });
+}
